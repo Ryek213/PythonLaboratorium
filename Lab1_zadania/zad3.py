fa59a62a04298c36c0d8f17c73f01b17a0223f75
Arkadiusz Rymarz <arek59rymarz@gmail.com>
2023-11-05 20:48:36 +0100

    Skończone zadanie 5

--- Lab1_zadania/zad3.py
+++ Lab1_zadania/zad3.py
@@ -1,8 +1,7 @@
-x=int(input("Podaj pierwszy bok prostokąta: "))
-y=int(input("Podaj drugi bok: "))
+x = int(input("Podaj pierwszy bok prostokąta: "))
+y = int(input("Podaj drugi bok: "))
 
 pole = x*y
 obwod = x*2 + y*2
 
-print("Pole prostokąta jest równe: ", pole)
-print("Obwód jest równy: ", obwod)
+print(f"Pole prostokąta jest równe: {pole}, a obwód jest równy: {obwod}")
--- Lab1_zadania/zad4.py
+++ Lab1_zadania/zad4.py
@@ -1,11 +1,11 @@
 import random
 
 droga = random.randint(1, 100000)
-print("Pokonana droga:", droga, "km")
+print(f"Pokonana droga: {droga}km")
 spalanie = float(input("Podaj średnie spalanie samochodu (l/100km): "))
 
 zuzycie = droga * spalanie / 100
 koszty = zuzycie * 6.5
 
-print("Przewidywanie zużycie paliwa:", zuzycie, "litrów")
-print("Szacowane koszty podróży:", koszty, "złotych")
+print(f"Przewidywanie zużycie paliwa: {zuzycie}litrów")
+print(f"Szacowane koszty podróży: {koszty}złotych")
