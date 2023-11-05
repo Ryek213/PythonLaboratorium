7b3e2ce94c365f443a2ecb34875defb971a0eeb4
Arkadiusz Rymarz <arek59rymarz@gmail.com>
2023-11-05 20:26:17 +0100

    Skończone zadanie 3

---
+++ Lab1_zadania/zad3.py
@@ -0,0 +1,8 @@
+x=int(input("Podaj pierwszy bok prostokąta: "))
+y=int(input("Podaj drugi bok: "))
+
+pole = x*y
+obwod = x*2 + y*2
+
+print("Pole prostokąta jest równe: ", pole)
+print("Obwód jest równy: ", obwod)
