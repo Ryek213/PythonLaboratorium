21bf506996173a60a3685998fe0820b1af134271
Arkadiusz Rymarz <arek59rymarz@gmail.com>
2023-11-05 21:33:56 +0100

    Skończone zadanie dodatkowe 3

---
+++ Lab1_zadania/zadDodatkowe3.py
@@ -0,0 +1,7 @@
+a = float(input("Podaj pierwszą liczbę: "))
+b = float(input("Podaj drugą liczbę: "))
+
+print(f"Dodawanie: {a+b}")
+print(f"Odejmowanie: {a-b}")
+print(f"Mnożenie: {a*b}")
+print(f"Dzielenie: {a/b}")
