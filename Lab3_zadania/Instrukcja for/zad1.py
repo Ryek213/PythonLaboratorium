a9af1cd09dd680c0ff68f1f0123b96de9fa5363b
w69998 <arek59rymarz@gmail.com>
2023-11-13 12:27:38 +0100

    Skończono zadanie 1 z instrukcji for

---
+++ Lab3_zadania/Instrukcja for/zad1.py
@@ -0,0 +1,15 @@
+for i in range(100):
+    print(i+1, end=" ")
+print('\n')
+
+for i in range(100, 0-1, -1):
+    print(i, end=" ")
+print('\n')
+
+for i in range(7, 77-1, 7):
+    print(i, end=" ")
+print('\n')
+
+for i in range(20, 0-1, -2):
+    print(i, end=" ")
+print('\n')
