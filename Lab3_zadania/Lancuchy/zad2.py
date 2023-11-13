0e9a622c046bfdd71f721fa54b3c59d0603fc742
w69998 <arek59rymarz@gmail.com>
2023-11-13 13:19:29 +0100

    Skończono zadanie 2 z łańcuchów

---
+++ Lab3_zadania/Lancuchy/zad2.py
@@ -0,0 +1,8 @@
+str = "Python jest super"
+
+print(str[0])
+print(str[-1])
+print(str[0: len(str)+1: 2])
+print(str[1: len(str)+1: 3])
+print(str[10: len(str)+1])
+print(str[-1: 0: -1], str[0], sep="")
