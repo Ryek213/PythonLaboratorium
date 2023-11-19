3fb5510676bcebb14a7c6e2de53c3912d9846e91
Arkadiusz Rymarz <arek59rymarz@gmail.com>
2023-11-19 21:11:52 +0100

    Poprawka w nazwie zmiennej

--- Lab3_zadania/Lancuchy/zad2.py
+++ Lab3_zadania/Lancuchy/zad2.py
@@ -1,8 +1,8 @@
-str = "Python jest super"
+tekst = "Python jest super"
 
-print(str[0])
-print(str[-1])
-print(str[0: len(str)+1: 2])
-print(str[1: len(str)+1: 3])
-print(str[10: len(str)+1])
-print(str[-1: 0: -1], str[0], sep="")
+print(tekst[0])
+print(tekst[-1])
+print(tekst[0: len(tekst) + 1: 2])
+print(tekst[1: len(tekst) + 1: 3])
+print(tekst[10: len(tekst) + 1])
+print(tekst[-1: 0: -1], tekst[0], sep="")
