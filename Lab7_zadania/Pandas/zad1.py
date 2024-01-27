ea28786f73454d03e63db1451f71aa5a8d5e84d3
Ryek213 <arek59rymarz@gmail.com>
2024-01-27 17:54:08 +0100

    Aktualizacja zadania 1 z Lab 7

--- Lab7_zadania/Pandas/zad1.py
+++ Lab7_zadania/Pandas/zad1.py
@@ -1,6 +1,7 @@
+import numpy as np
 import pandas as pd
 
-data = {'Nr_Albumu': ['12345', '54123', '98745', '64985'],
+data = {'Nr_Albumu': [12345, 54123, 98745, 64985],
         'Imię': ['Anna', 'Jan', 'Katarzyna', 'Tomasz'],
         'Nazwisko': ['Kowalska', 'Nowak', 'Wójcik', 'Ferenc'],
         'Ocena': [4.0, 5.0, 3.5, 4.0],
@@ -28,6 +29,50 @@ for ocena in dfgOcena.first().index:
         suma += df.loc[index, "Wiek"]
         ile += 1
     print(f"Średnia wieku dla oceny {ocena}: {suma/ile}")
+print('\n')
 
 # d
 print("d:")
+poprawa = {'Nr_Albumu': [12345, 98745, 64985],
+        'Ocena_poprawa': [4.5, 4.0, 4.0]}
+df_poprawa = pd.DataFrame(poprawa)
+print(df_poprawa)
+
+df = df.join(df_poprawa.set_index("Nr_Albumu"), on="Nr_Albumu")
+print(df)
+print('\n')
+
+# e
+print("e:")
+df.to_csv("df.csv", index = False)
+print("Zapisano DataFrame df do pliku df.csv")
+print('\n')
+
+# f
+print("f:")
+df1 = pd.read_csv("df.csv")
+print("Wczytano plik df.csv do zmiennej df1")
+print(df1)
+
+if len(df1.compare(df).index) == 0:
+    print("Ramki danych są takie same")
+else:
+    print("Ramki danych różnią się")
+    df1.compare(df)
+print('\n')
+
+# g
+print("g:")
+
+df.loc[len(df)] = [54647, "Maksymilian", "Kowalski", 5.0, 30, np.NaN]
+print(f"Nowy student:\n {df.loc[4]}")
+print('\n')
+
+# h
+print("h:")
+print("Unikalne wartości:", df["Ocena"].unique().tolist())
+print('\n')
+
+# i
+print("i:")
+print(f"Liczba studentów z oceną 5.0: {len(df[df['Ocena'] == 5])}")
